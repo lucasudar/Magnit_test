d3d69a85a12e789b78fb5b6ab1a14be129d61065
_Lucas_ <lucasudar@gmail.com>
2021-07-27 18:45:33 +0300

    added asking popup before close plate

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -2,10 +2,14 @@
   <div class="plates">
     <p v-if="!plates.length">There are no data yet.</p>
     <v-card
+        class="block"
         v-for="plate in plates"
         :key="plate"
         elevation="1"
     >
+      <button type="button" class="close"
+              @click="areYouSure"> x
+      </button>
       <v-card-title>{{ plate.selectedRate }}</v-card-title>
       <v-card-subtitle>{{ plate.price }}</v-card-subtitle>
       <v-card-text>{{ plate.currentTimeStamp }}</v-card-text>
@@ -28,9 +32,19 @@ export default {
     plates: [],
   }),
   methods: {
-
+    closePlate: function () {
+      let plates = document.querySelectorAll('.block');
+      for (let plate of plates) {
+        plate.firstChild.onclick = () => plate.remove();
+      }
+    },
+    areYouSure: function () {
+      if (confirm("Are you sure?")) this.closePlate ()
+      else return;
+    }
   },
   components: {
+
   },
 }
 </script>
@@ -45,5 +59,16 @@ export default {
   width: 200px;
   height: 200px;
   border: 1px solid;
+}
+.close {
+  position: absolute;
+  right: 1px;
+  margin-bottom: 15px;
+  width: 32px;
+  height: 32px;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+  background: transparent;
 }
 </style>

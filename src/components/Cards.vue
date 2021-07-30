d397541089801db7657f14f8b06e7e2da4238384
_Lucas_ <lucasudar@gmail.com>
2021-07-30 14:40:42 +0300

    added deleting card future

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -3,12 +3,12 @@
     <p v-if="!plates.length">There are no data yet.</p>
     <v-card
         class="block"
-        v-for="plate in plates"
+        v-for="(plate, index) in plates"
         :key="plate"
         elevation="1"
     >
       <button type="button" class="close"
-              @click="areYouSure"> x
+              @click="areYouSure(index)"> x
       </button>
       <v-card-title>{{ plate.selectedRate }}</v-card-title>
       <v-card-subtitle>{{ plate.price }}</v-card-subtitle>
@@ -21,7 +21,7 @@
 export default {
   name: "Cards",
   props: {
-    card: Object
+    card: Object,
   },
   watch: {
     card: function () {
@@ -29,11 +29,12 @@ export default {
         this.plates.push(this.card)
       } else {
         let allRates = [];
-        this.plates.forEach(obj => allRates.push(Object.values(obj).slice(0,1).join()))
+        this.plates.forEach(obj => allRates.push(Object.values(obj).slice(0, 1).join()))
         if (allRates.includes(this.card.selectedRate)) {
           alert('Can\'t add, this is a copy!')
         } else {
           this.plates.push(this.card)
+          console.log(this.plates[1])
         }
       }
     },
@@ -42,17 +43,15 @@ export default {
     plates: [],
   }),
   methods: {
-    areYouSure: function () {
+    areYouSure: function (index) {
       if (confirm("Are you sure?")) {
-        let plates = document.querySelectorAll('.block');
-        for (let plate of plates) {
-          plate.firstChild.onclick = () => plate.remove();
-        }
+        this.plates.splice(index, 1)
       } else return;
     }
   },
   components: {},
-}
+  }
+
 </script>
 
 <style scoped>
